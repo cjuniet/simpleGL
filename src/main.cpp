7f16747ae4550105723c1c864a8adbffeccd67dc
Christophe Juniet <c.juniet@gmail.com>
2014-12-02 00:08:50 +0100

    Minor tweaks
    Fix C++11 flag.
    Add exit on Escape.
    Clear the window.
    Make the window non-resizable.
    Fix viewport for hdpi.

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ int main(int argc, char* argv[])
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
   GLFWwindow* window = glfwCreateWindow(800, 600, "SimlpeGL", nullptr, nullptr);
   if (!window) {
@@ -23,8 +24,18 @@ int main(int argc, char* argv[])
     return -1;
   }
 
+  int width, height;
+  glfwGetFramebufferSize(window, &width, &height);
+  glViewport(0, 0, width, height);
+
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+      glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
     glfwSwapBuffers(window);
   }
